4f30767ae396898c325ff231a05cc91a0d781344
aRhezaHarliman <rheza.harliman@access-company.com>
2018-05-09 14:08:04 +0900

    Imp : Add test code for show question

--- test/web/controller/question/show_test.exs
+++ test/web/controller/question/show_test.exs
@@ -1,7 +1,34 @@
 defmodule StackoverflowCloneB.Controller.Question.ShowTest do
   use StackoverflowCloneB.CommonCase
+  alias Sazabi.G2gClient
+  alias StackoverflowCloneB.TestData.QuestionData
 
-  test "show/1 " do
-    # Implement me
+  @api_prefix "/v1/question/question_id"
+
+  test "show/1 " <>
+    "it returns question" do
+    :meck.expect(G2gClient, :send, fn(_, _, _) ->
+      %Dodai.RetrieveDedicatedDataEntitySuccess{body: QuestionData.dodai()}
+    end)
+
+    res = Req.get(@api_prefix)
+    assert res.status               == 200
+    assert Poison.decode!(res.body) == QuestionData.gear()
+  end
+
+  test "show/1 " <>
+    "when specified question is not found " <>
+    "it returns ResourceNotFoundError" do
+    :meck.expect(G2gClient, :send, fn(_, _, _) ->
+      %Dodai.ResourceNotFound{}
+    end)
+
+    res = Req.get(@api_prefix)
+    assert res.status               == 404
+    assert Poison.decode!(res.body) == %{
+      "code"        => "404-04",
+      "description" => "The resource does not exist in the database.",
+      "error"       => "ResourceNotFound",
+    }
   end
 end

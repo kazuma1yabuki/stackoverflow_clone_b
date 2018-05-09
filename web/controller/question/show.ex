21baed2e4ca821bd5bdb11c20bbde5b089226feb
aRhezaHarliman <rheza.harliman@access-company.com>
2018-05-09 13:46:36 +0900

    Imp : Add show question API

--- web/controller/question/show.ex
+++ web/controller/question/show.ex
@@ -1,7 +1,24 @@
 defmodule StackoverflowCloneB.Controller.Question.Show do
   use StackoverflowCloneB.Controller.Application
+  alias StackoverflowCloneB.Controller.Question.Helper
+  alias StackoverflowCloneB.Error.ResourceNotFoundError
 
-  def show(_conn) do
-    # Implement me
+  def show(conn) do
+
+    group_id = StackoverflowCloneB.Dodai.default_group_id()
+    id = conn.request.path_matches[:id]
+    root_key = StackoverflowCloneB.Dodai.root_key()
+    app_id = StackoverflowCloneB.Dodai.app_id()
+
+    request = Dodai.RetrieveDedicatedDataEntityRequest.new(group_id, Helper.collection_name(), id, root_key)
+    response = Sazabi.G2gClient.send(conn.context, app_id, request)
+
+    case response do
+      %Dodai.RetrieveDedicatedDataEntitySuccess{body: resp_body} 
+        -> Conn.json(conn, 200, Helper.to_response_body(resp_body))
+      %Dodai.ResourceNotFound{} 
+        -> ErrorJson.json_by_error(conn, ResourceNotFoundError.new())
+    end
+
   end
 end

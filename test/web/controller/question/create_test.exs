4403daec82910f73f83d431720464d6856c78c25
aRhezaHarliman <rheza.harliman@access-company.com>
2018-05-10 15:36:25 +0900

    Imp : Add test case for create question

--- test/web/controller/question/create_test.exs
+++ test/web/controller/question/create_test.exs
@@ -1,7 +1,34 @@
 defmodule StackoverflowCloneB.Controller.Question.CreateTest do
   use StackoverflowCloneB.CommonCase
+  alias Sazabi.G2gClient
+  alias StackoverflowCloneB.TestData.QuestionData
 
-  test "create/1 " do
-    # Implement me
+  @api_prefix "/v1/question"
+  @header     %{}
+  @body       %{"body" => "本文", "title" => "タイトル"}
+
+  test "create/1 " <>
+    "Create question test" do
+    :meck.expect(StackoverflowCloneB.Plug.FetchMe, :fetch, fn(conn, _) ->
+      SolomonLib.Conn.assign(conn, :me, StackoverflowCloneB.TestData.UserData.dodai())
+    end)      
+    :meck.expect(G2gClient, :send, fn(_, _, req) ->
+      assert req.body == %Dodai.CreateDedicatedDataEntityRequestBody{
+        data: %{
+          "comments"        => [],
+          "like_voter_ids"    => [],
+          "dislike_voter_ids" => [],
+          "title" => "タイトル", 
+          "body" => "本文",
+          "user_id" => "user_id",}
+      }
+      IO.inspect req.body
+      %Dodai.CreateDedicatedDataEntitySuccess{body: QuestionData.dodai()}
+    end)
+
+    res = Req.post_json(@api_prefix, @body, @header)
+    IO.inspect res.status
+    assert res.status               == 201
+    assert Poison.decode!(res.body) == QuestionData.gear()
   end
 end

d79f33ab1729a7c342c247a58f6c6420c97eae9a
aRhezaHarliman <rheza.harliman@access-company.com>
2018-05-14 13:15:56 +0900

    Imp : Add query test for answer

--- test/web/controller/answer/index_test.exs
+++ test/web/controller/answer/index_test.exs
@@ -1,7 +1,40 @@
 defmodule StackoverflowCloneB.Controller.Answer.IndexTest do
   use StackoverflowCloneB.CommonCase
+  alias Dodai.RetrieveDedicatedDataEntityListRequestQuery, as: Query
+  alias StackoverflowCloneB.TestData.QuestionData
+  alias StackoverflowCloneB.Controller.Answer.{Index, IndexRequestParams}
 
-  test "create/1 " do
-    # Implement me
+  @api_prefix "/v1/answer"
+
+  test "indedx/1 " <>
+    "should return answer" do
+    :meck.expect(Sazabi.G2gClient, :send, fn(_, _, req) ->
+      assert req.query == %Dodai.RetrieveDedicatedDataEntityListRequestQuery{
+        limit: nil,
+        skip:  nil,
+        query: %{},
+        sort:  %{"_id" => 1}
+      }
+
+      %Dodai.RetrieveDedicatedDataEntityListSuccess{body: [QuestionData.dodai()]}
+    end)
+
+    res = Req.get(@api_prefix)
+    assert res.status               == 200
+    assert Poison.decode!(res.body) == [QuestionData.gear()]
+  end
+
+  test "index/1 " <>
+  "should build answer query" do
+    params_list = [
+      {%IndexRequestParams{user_id: nil,       question_id: nil,           body: nil   }, %Query{query: %{                                                                                       }, sort: %{"_id" => 1}}},
+      {%IndexRequestParams{user_id: nil,       question_id: "question_id", body: nil   }, %Query{query: %{                             "data.question_id" => "question_id"                       }, sort: %{"_id" => 1}}},
+      {%IndexRequestParams{user_id: nil,       question_id: nil,           body: "body"}, %Query{query: %{                                                                  "data.body" => "body"}, sort: %{"_id" => 1}}},
+      {%IndexRequestParams{user_id: "user_id", question_id: nil,           body: nil   }, %Query{query: %{"data.user_id" => "user_id"                                                            }, sort: %{"_id" => 1}}},
+      {%IndexRequestParams{user_id: "user_id", question_id: "question_id", body: "body"}, %Query{query: %{"data.user_id" => "user_id", "data.question_id" => "question_id", "data.body" => "body"}, sort: %{"_id" => 1}}},
+    ]
+    Enum.each(params_list, fn {params, expected} ->
+      assert Index.convert_to_dodai_req_query(params) == expected
+    end)
   end
 end
